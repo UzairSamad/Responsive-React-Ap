aabd78d183a217c5edfea1cb351c1b28b747f1cb
M.Uzair <uzair.iu17@gmail.com>
2020-05-10 03:03:41 +0500

    added responsive comment component

--- src/components/DishDetailed.js
+++ src/components/DishDetailed.js
@@ -1,35 +1,75 @@
 import React, { Component } from 'react'
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component{
+class DishDetail extends Component {
 
-    renderDish(dish) {
-        console.log(this.props)
-
-        if (dish != null)
-            return(
+    renderComments(dish,comments) {
+        if (comments != null)
+            return (
                 <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
                     <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
+                        <CardTitle> <h4  class="text-sm-left" >Comments</h4></CardTitle>
+                        <CardText>
+                            <ul class="list-unstyled">
+                                {
+                                    comments.map((val, index) => {
+                                        return (
+                                            <div>
+                                                <li  class="text-sm-left pb-2" > {val.comment} </li>
+                                                <li class="text-sm-left pb-3" paddi> --{val.author} </li>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </CardText>
                     </CardBody>
                 </Card>
             );
         else
-            return(
+            return (
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>No Comments Yet For this</CardTitle>
+                    </CardBody>
+                </Card>
+            )
+
+    }
+    renderDish(dish) {
+        console.log(this.props)
+
+        if (dish != null)
+            return (
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+
+
+            );
+        else
+            return (
                 <div></div>
             );
-      }
-    render(){
-        return(
+    }
+    render() {
+        return (
             <div className="container">
-            <div className="row">
-              <div  className="col-12 col-md-5 m-1">
-                {this.renderDish(this.props.selectedDish)}
-              </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        {this.renderDish(this.props.selectedDish)}
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        {this.renderComments(this.props.selectedDish,this.props.selectedDish.comments)}
+                    </div>
+
+                </div>
             </div>
-        </div>
         )
     }
 }
