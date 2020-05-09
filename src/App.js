4e6438c28962cd6b04a2ccca2e2ed63515bed343
M.Uzair <uzair.iu17@gmail.com>
2020-05-10 02:03:30 +0500

    added card in the menu

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Menu from './components/Menu'
 import Navigation from './components/Navbar'
+import { DISHES } from './shared/dishes';
 
 
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render(){ 
   return (
     <div className="App">
       <Navigation />
+      <Menu dishes={this.state.dishes} />
     </div>
   );
+   }
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,14 +11,13 @@ class Navigation extends Component {
 
     navigationItems =
         this.navigationItemsData.map((val, index) => {
-            return <li style={{ marginRight: '20px', listStyle: 'none',color:'white' ,paddingTop:'5px'}} key={val.id}> {val.name} </li>
+            return <li style={{ marginRight: '20px', listStyle: 'none',color:'white' ,paddingTop:'5px',cursor:'pointer'}} key={val.id}> {val.name} </li>
         })
-
 
     render() {
         return (
-            <div className="App">
-                <Navbar dark color="primary">
+            <div >
+                <Navbar dark color="primary"  >
                     <div className="container" style={{ display: 'flex', flexDirection: 'row' }} >
                         <NavbarBrand href="/">E-Commerce Solutions</NavbarBrand>
                         <ul style={{ display: 'flex' }}>
