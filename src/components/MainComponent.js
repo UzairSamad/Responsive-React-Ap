049896aae443e2460fdae57864c60296cd56931d
M.Uzair <uzair.iu17@gmail.com>
2020-05-11 02:46:25 +0500

    added footer and css classes

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,35 +1,38 @@
 import React, { Component } from 'react';
 import Menu from './Menu'
 import Header from './Header'
+import Footer from './Footer'
 import DishDetailed from './DishDetailed'
 
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        dishes: DISHES,
-        selectedDish: null
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            selectedDish: null
+        };
+    }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
+    onDishSelect(dishId) {
+        this.setState({ selectedDish: dishId });
+    }
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) =>  this.onDishSelect(dishId)} />
-        { this.state.selectedDish != null &&
-        <DishDetailed  dish={this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish )} />
-        }
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Header />
+                <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+                {this.state.selectedDish != null &&
+                    <DishDetailed dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)} />
+                }
+                <Footer />
+
+            </div>
+        );
+    }
 }
 
 export default Main;
